737a9822aaaf4b3f05652fc26f05cfa9c875a970
Mikhail <mvb.nnov@mail.ru>
2023-01-30 19:02:46 +0300

    auth v1.1

--- src/main/java/com/colvir/j3/store/config/auth/impl/AuthServiceImplementation.java
+++ src/main/java/com/colvir/j3/store/config/auth/impl/AuthServiceImplementation.java
@@ -1,6 +1,6 @@
 package com.colvir.j3.store.config.auth.impl;
 
-//import io.jsonwebtoken.*;
+import io.jsonwebtoken.*;
 import com.colvir.j3.store.config.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -69,38 +69,38 @@ public class AuthServiceImplementation implements AuthService {
     public String generateToken(Authentication authentication) {
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
-//        return Jwts.builder()
-//                .setSubject((userPrincipal.getUsername()))
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
-//                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-//                .compact();
-        return userPrincipal.getUsername();
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+//        return userPrincipal.getUsername();
     }
 
     public String getUserNameFromToken(String token) {
-//        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
-        return token;
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+//        return token;
     }
 
     public boolean tokenIsValid(final String authToken) {
-//        try {
-////            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
-//            return true;
-////        } catch (SignatureException e) {
-//////            logger.error("Invalid JWT signature: {}", e.getMessage());
-////        } catch (MalformedJwtException e) {
-//////            logger.error("Invalid JWT token: {}", e.getMessage());
-////        } catch (ExpiredJwtException e) {
-//////            logger.error("JWT token is expired: {}", e.getMessage());
-////        } catch (UnsupportedJwtException e) {
-//////            logger.error("JWT token is unsupported: {}", e.getMessage());
-////        } catch (IllegalArgumentException e) {
-//////            logger.error("JWT claims string is empty: {}", e.getMessage());
-//        }
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+//            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+//            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+//            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+//            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+//            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
 
-//        return false;
-        return true;
+        return false;
+//        return true;
 
     }
 
